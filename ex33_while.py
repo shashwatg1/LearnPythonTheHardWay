57a3c3706574658fe3f5a65be74aecabb6c1c2f4
shashwatg1 <shashwatg1@gmail.com>
2016-05-25 15:58:07 +0530

    Completed

---
+++ ex33_while.py
@@ -0,0 +1,36 @@
+# ex33: While Loops
+
+i = 0
+numbers = []
+
+while i < 6:
+    print "At the top i is %d" % i
+    numbers.append(i)
+    i = i + 1
+    print "Numbers now: ", numbers
+    print "At the bottom i is %d" % i
+
+print "The numbers: "
+
+for num in numbers:
+    print num
+
+print "------------"
+
+def createNumbers(max, step):
+    i = 0
+    numbers = []
+    for i in range(0, max, step):
+        print "At the top i is %d" % i
+        numbers.append(i)
+
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
+    return numbers
+
+numbers = createNumbers(11, 2)
+
+print "The numbers: "
+
+for num in numbers:
+    print num
